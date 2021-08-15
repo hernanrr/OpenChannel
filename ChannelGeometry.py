6e901a6929eca277b4034d242b234f8eced17fc3
Ricardo Hernandez <ricardo.hernandezmoreira@gmail.com>
2021-08-15 19:10:09 -0400

    Created rectangular cross-section generator and unittests

--- ChannelGeometry.py
+++ ChannelGeometry.py
@@ -1,4 +1,25 @@
 #!/usr/bin/env python3
 
+import numpy as np
+
+
+class Rectangular:
+    def __init__(self, width, depth):
+        width = np.array(width)
+        depth = np.array(depth)
+
+        if width.dtype.type is np.str_ or np.all(width <= 0):
+            raise ValueError('Width must be a positive number')
+        if depth.dtype.type is np.str_ or np.all(depth <= 0):
+            raise ValueError('Depth must be a positive number')
+
+        self.width = width
+        self.depth = depth
+
+
+def main():
+    pass
+
+
 if __name__ == '__main__':
     main()
---
+++ test_ChannelGeometry.py
@@ -0,0 +1,35 @@
+import unittest
+import ChannelGeometry as cg
+
+class TestChannelGeometry(unittest.TestCase):
+
+    def test_create_rectangular_channel(self):
+        self.rectangular = cg.Rectangular(width=10, depth=2)
+
+        self.rectangular = cg.Rectangular(width=[10, 5], depth=[2, 1.5])
+
+    def test_create_rectangular_channel_invalid_size(self):
+        with self.assertRaises(ValueError):
+            cg.Rectangular(width=0, depth=2)
+
+        with self.assertRaises(ValueError):
+            cg.Rectangular(width=-2.5, depth=2)
+
+        with self.assertRaises(ValueError):
+            cg.Rectangular(width='foo', depth=2)
+
+        with self.assertRaises(ValueError):
+            cg.Rectangular(width=10, depth=0)
+
+        with self.assertRaises(ValueError):
+            cg.Rectangular(width=10, depth=-2.5)
+
+        with self.assertRaises(ValueError):
+            cg.Rectangular(width=-2.5, depth='foo')
+
+        with self.assertRaises(ValueError):
+            cg.Rectangular(width=-2.5, depth=-2)
+
+
+if __name__ == '__main__':
+    unittest.main()
