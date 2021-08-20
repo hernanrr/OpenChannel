0374eadef736451f12c403b7843cfde0b121c91c
Ricardo Hernandez <ricardo.hernandezmoreira@gmail.com>
2021-08-20 01:09:39 -0400

    Added functions for Trapezoidal channel.

--- ChannelGeometry.py
+++ ChannelGeometry.py
@@ -58,6 +58,32 @@ class Triangular(ChannelXSection):
         return 8 / (3 * self.depth)
 
 
+class Trapezoidal(ChannelXSection):
+    def __init__(self, width, depth, slope):
+        self.width = width
+        self.depth = depth
+        self.slope = slope
+
+    def area(self):
+        return (self.width + self.slope * self.depth) * self.depth
+
+    def perimeter(self):
+        return (self.width
+                + 2 * self.depth * math.sqrt(1 + self.slope ** 2))
+
+    def top_width(self):
+        return self.width + 2 * self.depth * self.slope
+
+    def shape_function(self):
+        A = math.sqrt(1 + self.slope ** 2)
+        num = (self.top_width
+               * (5 * self.width + 6 * self.depth * A)
+               * (4 * self.slope * self.depth ** 2 * A))
+        denom = (3 * self.depth * (self.width + self.depth * self.slope)
+                 * (self.width + 2 * self.depth * A))
+        return num / denom
+
+
 def main():
     pass
 
