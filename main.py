02069f275630b8dd8e8646d7303c40f06e3e4b98
Ricardo Hernández <ricardo.hernandezmoreira@gmail.com>
2021-08-22 22:53:06 -0400

    Added logging functionality.

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
 #!/usr/bin/env python3
+
+""""This is the main file."""
+
+import logging
+
+
+def main():
+    logging.basicConfig(filename="main.log",
+                        format='%(asctime)s %(levelname)s: %(message)s',
+                        filemode="w",
+                        level=logging.INFO)
+    logging.info('Started')
+
+    logging.info('Finished')
+
+
+if __name__ == "__main__":
+    main()
+ 
