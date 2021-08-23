536a9bf0b9b33e34de64c8131a22ceec525f3817
Ricardo Hernández <ricardo.hernandezmoreira@gmail.com>
2021-08-23 12:04:53 -0400

    Added logging functionality to main, as a wrapper.

--- main.py
+++ main.py
@@ -3,18 +3,15 @@
 """"This is the main file."""
 
 import logging
+from logconfig import log_actions
 
 
+@log_actions
 def main():
-    logging.basicConfig(filename="main.log",
-                        format='%(asctime)s %(levelname)s: %(message)s',
-                        filemode="w",
-                        level=logging.INFO)
     logging.info('Started')
-
+    print('Hello world!')
     logging.info('Finished')
 
 
 if __name__ == "__main__":
     main()
- 
